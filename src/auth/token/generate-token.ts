2ef454ac8bc9b793c282187a324bf12e4877c588
mayjunjuly0313 <tank6138@gmail.com>
2021-07-17 10:36:53 +0900

    feat: added generate-token method

---
+++ src/auth/token/generate-token.ts
@@ -0,0 +1,26 @@
+import jwt, { SignOptions } from 'jsonwebtoken'
+
+const { JWT_SECRET } = process.env
+
+export async function generateToken(
+  payload: string | Record<string, unknown> | Buffer,
+  options: SignOptions = {}
+): Promise<string> {
+  if (!JWT_SECRET) {
+    throw new Error('JWT_SECRET Variable is not set')
+  }
+  const promise = new Promise<string>((resolve, reject) => {
+    jwt.sign(payload, JWT_SECRET, options, (error, token) => {
+      if (error) {
+        reject(error)
+        return
+      }
+      if (!token) {
+        reject(new Error('Failed to generate token'))
+        return
+      }
+      resolve(token)
+    })
+  })
+  return promise
+}
