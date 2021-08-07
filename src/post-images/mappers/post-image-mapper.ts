27a60c7e8377946c63fea5a03131375a37049fce
mayjunjuly0313 <tank6138@gmail.com>
2021-08-07 22:36:41 +0900

    feat: added post-images mapper between model and domain

---
+++ src/post-images/mappers/post-image-mapper.ts
@@ -0,0 +1,36 @@
+import { UniqueEntityId } from 'core/infra/unique-entity-id'
+import { PostImageModel } from 'infra/models/post-image-model'
+import { PostImage } from 'post-images/domain/post-image'
+import { ExtensionType, Extension } from '../domain/extension'
+
+export class PostImageMapper {
+  public static toPersistence(postImage: PostImage): any {
+    return {
+      postId: postImage.postId.toString(),
+      originalName: postImage.originalName,
+      imageUrl: postImage.imageUrl,
+      thumbnailUrl: postImage.thumbnailUrl,
+      isFirstPick: postImage.isFirstPick,
+      extension: postImage.extension.value,
+    }
+  }
+
+  public static toDomain(postImageModel: PostImageModel): PostImage {
+    const postId = new UniqueEntityId(postImageModel.postId)
+    const extension = new Extension(postImageModel.extension as ExtensionType)
+
+    const postImage = new PostImage(
+      {
+        postId: postId,
+        originalName: postImageModel.originalName,
+        imageUrl: postImageModel.imageUrl,
+        thumbnailUrl: postImageModel.thumbnailUrl,
+        isFirstPick: postImageModel.isFirstPick,
+        extension: extension,
+      },
+      new UniqueEntityId(postImageModel.id)
+    )
+
+    return postImage
+  }
+}
