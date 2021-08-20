037319c5345cc4df1ab58f203e7ad35455050fd4
mayjunjuly0313 <tank6138@gmail.com>
2021-08-20 13:55:47 -0400

    refactor: added try catch block in use-cases

--- src/post-images/use-cases/create-images/create-post-images.ts
+++ src/post-images/use-cases/create-images/create-post-images.ts
@@ -22,63 +22,67 @@ export class CreatePostImages {
   ) {}
 
   public async execute(inputDto: CreatePostImagesInputDto): Promise<Response> {
-    inputDto.imageFiles.forEach(async (imageFile, idx) => {
-      if (Extension.isExtension(imageFile.mimetype)) {
-        return new CreateImagesError.InvalidExtension()
-      }
-      const extension = new Extension(imageFile.mimetype as ExtensionType)
-
-      let imageUrl, thumbnailUrl
-      try {
-        imageUrl = (await s3upload(imageFile)) as string
-
-        const thumbnailImage: ImageFile = {
-          originalname: `thumbnail_${imageFile.originalname}`,
-          mimetype: imageFile.mimetype,
-          buffer: await sharp(imageFile.buffer)
-            .resize({ width: 270, height: 270, fit: 'inside' })
-            .toBuffer(),
+    try {
+      inputDto.imageFiles.forEach(async (imageFile, idx) => {
+        if (Extension.isExtension(imageFile.mimetype)) {
+          return new CreateImagesError.InvalidExtension()
         }
-        thumbnailUrl = (await s3upload(thumbnailImage)) as string
-      } catch (error) {
-        return new CreateImagesError.S3UploadFailed()
-      }
+        const extension = new Extension(imageFile.mimetype as ExtensionType)
 
-      const postId = new UniqueEntityId(inputDto.postId)
-      const originalName = imageFile.originalname
-      const isFirstPick = idx === inputDto.metadata.isFirstPick ? true : false
-      const width = inputDto.metadata.sizes[idx].width
-      const height = inputDto.metadata.sizes[idx].height
+        let imageUrl, thumbnailUrl
+        try {
+          imageUrl = (await s3upload(imageFile)) as string
 
-      const postImage = new PostImage({
-        originalName,
-        postId,
-        imageUrl,
-        thumbnailUrl,
-        isFirstPick,
-        extension,
-        width,
-        height,
+          const thumbnailImage: ImageFile = {
+            originalname: `thumbnail_${imageFile.originalname}`,
+            mimetype: imageFile.mimetype,
+            buffer: await sharp(imageFile.buffer)
+              .resize({ width: 270, height: 270, fit: 'inside' })
+              .toBuffer(),
+          }
+          thumbnailUrl = (await s3upload(thumbnailImage)) as string
+        } catch (error) {
+          return new CreateImagesError.S3UploadFailed()
+        }
+
+        const postId = new UniqueEntityId(inputDto.postId)
+        const originalName = imageFile.originalname
+        const isFirstPick = idx === inputDto.metadata.isFirstPick ? true : false
+        const width = inputDto.metadata.sizes[idx].width
+        const height = inputDto.metadata.sizes[idx].height
+
+        const postImage = new PostImage({
+          originalName,
+          postId,
+          imageUrl,
+          thumbnailUrl,
+          isFirstPick,
+          extension,
+          width,
+          height,
+        })
+
+        const alreadyExists = await this.postImageRepository.exists(postImage)
+        if (alreadyExists) {
+          return new CreateImagesError.PostImageExists()
+        }
+
+        try {
+          console.log('here')
+          await this.postImageRepository.save(postImage)
+        } catch (error) {
+          return new Error(
+            `postImage save failed, postId: ${inputDto.postId}, imageUrl: ${imageUrl}`
+          )
+        }
       })
 
-      const alreadyExists = await this.postImageRepository.exists(postImage)
-      if (alreadyExists) {
-        return new CreateImagesError.PostImageExists()
+      const outputDto: CreatePostImagesOutputDto = {
+        postId: inputDto.postId,
       }
-
-      try {
-        console.log('here')
-        await this.postImageRepository.save(postImage)
-      } catch (error) {
-        return new Error(
-          `postImage save failed, postId: ${inputDto.postId}, imageUrl: ${imageUrl}`
-        )
-      }
-    })
-
-    const outputDto: CreatePostImagesOutputDto = {
-      postId: inputDto.postId,
+      return outputDto
+    } catch (error) {
+      throw new Error(error.message)
     }
-    return outputDto
   }
 }
--- src/votes/use-cases/create-vote/create-vote.ts
+++ src/votes/use-cases/create-vote/create-vote.ts
@@ -19,44 +19,48 @@ export class CreateVote {
   ) {}
 
   public async execute(inputDto: CreateVoteInputDto): Promise<Response> {
-    if (!Category.isCategory(inputDto.category)) {
-      return new CreateVoteError.InvalidCategory()
-    }
-
-    const category = new Category(inputDto.category as CategoryType)
-    const userId = new UniqueEntityId(inputDto.userId)
-    const postId = new UniqueEntityId(inputDto.postId)
-    const postImageId = new UniqueEntityId(inputDto.postImageId)
-
-    const voteAlreadyExists =
-      await this.voteRepository.findVoteByUserIdAndPostId(userId, postId)
-    if (voteAlreadyExists) {
-      return new CreateVoteError.AlreadyExists()
-    }
-
-    const vote = new Vote({
-      userId: userId,
-      postId: postId,
-      postImageId: postImageId,
-      category: category,
-    })
-
-    console.log(postId)
-    console.log(postImageId)
-    console.log(userId)
-
     try {
-      await this.voteRepository.save(vote)
-    } catch (error) {
-      return new Error(
-        `vote save failed, userId: ${userId.toString()}, postId: ${postId.toString()}`
-      )
-    }
+      if (!Category.isCategory(inputDto.category)) {
+        return new CreateVoteError.InvalidCategory()
+      }
 
-    const outputDto: CreateVoteOutputDto = {
-      userId: userId.toString(),
-      postId: postId.toString(),
+      const category = new Category(inputDto.category as CategoryType)
+      const userId = new UniqueEntityId(inputDto.userId)
+      const postId = new UniqueEntityId(inputDto.postId)
+      const postImageId = new UniqueEntityId(inputDto.postImageId)
+
+      const voteAlreadyExists =
+        await this.voteRepository.findVoteByUserIdAndPostId(userId, postId)
+      if (voteAlreadyExists) {
+        return new CreateVoteError.AlreadyExists()
+      }
+
+      const vote = new Vote({
+        userId: userId,
+        postId: postId,
+        postImageId: postImageId,
+        category: category,
+      })
+
+      console.log(postId)
+      console.log(postImageId)
+      console.log(userId)
+
+      try {
+        await this.voteRepository.save(vote)
+      } catch (error) {
+        return new Error(
+          `vote save failed, userId: ${userId.toString()}, postId: ${postId.toString()}`
+        )
+      }
+
+      const outputDto: CreateVoteOutputDto = {
+        userId: userId.toString(),
+        postId: postId.toString(),
+      }
+      return outputDto
+    } catch (error) {
+      throw new Error(error.message)
     }
-    return outputDto
   }
 }
