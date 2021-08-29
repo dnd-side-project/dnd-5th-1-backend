0d45ed9c67b768a8637f7baeffbc2a73da56309e
mayjunjuly0313 <tank6138@gmail.com>
2021-08-28 20:28:59 -0400

    ci: change vpc endpoint setting and set to use SecretsManager instead of ssm

--- infrastructures/lib/credentials-stack.ts
+++ infrastructures/lib/credentials-stack.ts
@@ -1,15 +1,16 @@
-import * as ssm from '@aws-cdk/aws-ssm'
 import * as cdk from '@aws-cdk/core'
+import * as ecs from '@aws-cdk/aws-ecs'
+import { Secret } from '@aws-cdk/aws-secretsmanager'
 
 export interface S3Credentials {
   S3_BUCKET_NAME: string
-  ACCESS_KEY_ID: ssm.IStringParameter
-  SECRET_ACCESS_KEY: ssm.IStringParameter
+  ACCESS_KEY_ID: ecs.Secret
+  SECRET_ACCESS_KEY: ecs.Secret
 }
 
 export interface AppCredentials {
   PORT: string
-  JWT_SECRET: ssm.IStringParameter
+  JWT_SECRET: ecs.Secret
 }
 
 interface PickmeCredentials {
@@ -24,53 +25,57 @@ export class CredentialsStack extends cdk.Stack {
     super(scope, id, props)
 
     // import App Credentials
-    const appPort = ssm.StringParameter.fromStringParameterAttributes(
+    const appPort = Secret.fromSecretPartialArn(
       this,
-      '/pickme/port',
-      {
-        parameterName: '/pickme/port',
-        simpleName: false,
-      }
+      '/pickme/app',
+      'arn:aws:secretsmanager:ap-northeast-2:369590600858:secret:/pickme/app-5FDYDf'
+    )
+      .secretValueFromJson('port')
+      .toString()
+
+    const appJwtSecret = ecs.Secret.fromSecretsManager(
+      Secret.fromSecretPartialArn(
+        this,
+        '/pickme/app',
+        'arn:aws:secretsmanager:ap-northeast-2:369590600858:secret:/pickme/app-5FDYDf'
+      ),
+      'jwt_secret'
     )
 
-    const appJwtSecret =
-      ssm.StringParameter.fromSecureStringParameterAttributes(
-        this,
-        '/pickme/jwt_secret',
-        { parameterName: '/pickme/jwt_secret', version: 1 }
-      )
-
     const appCredentials: AppCredentials = {
-      PORT: appPort.stringValue,
+      PORT: appPort,
       JWT_SECRET: appJwtSecret,
     }
 
     // import S3 Credentials
-    const s3BucketName = ssm.StringParameter.fromStringParameterAttributes(
+    const s3BucketName = Secret.fromSecretPartialArn(
       this,
-      '/pickme/s3/bucket_name',
-      {
-        parameterName: '/pickme/s3/bucket_name',
-        simpleName: false,
-      }
+      '/pickme/s3',
+      'arn:aws:secretsmanager:ap-northeast-2:369590600858:secret:/pickme/s3-SIuNEM'
+    )
+      .secretValueFromJson('bucket_name')
+      .toString()
+
+    const s3AccessKeyId = ecs.Secret.fromSecretsManager(
+      Secret.fromSecretPartialArn(
+        this,
+        '/pickme/s3',
+        'arn:aws:secretsmanager:ap-northeast-2:369590600858:secret:/pickme/s3-SIuNEM'
+      ),
+      'access_key_id'
     )
 
-    const s3AccessKeyId =
-      ssm.StringParameter.fromSecureStringParameterAttributes(
+    const s3SecretAccessKey = ecs.Secret.fromSecretsManager(
+      Secret.fromSecretPartialArn(
         this,
-        '/pickme/s3/access_key_id',
-        { parameterName: '/pickme/s3/access_key_id', version: 1 }
-      )
-
-    const s3SecretAccessKey =
-      ssm.StringParameter.fromSecureStringParameterAttributes(
-        this,
-        '/pickme/s3/secret_access_key',
-        { parameterName: '/pickme/s3/secret_access_key', version: 1 }
-      )
+        '/pickme/s3',
+        'arn:aws:secretsmanager:ap-northeast-2:369590600858:secret:/pickme/s3-SIuNEM'
+      ),
+      'secret_access_key'
+    )
 
     const s3Credentials: S3Credentials = {
-      S3_BUCKET_NAME: s3BucketName.stringValue,
+      S3_BUCKET_NAME: s3BucketName,
       ACCESS_KEY_ID: s3AccessKeyId,
       SECRET_ACCESS_KEY: s3SecretAccessKey,
     }
